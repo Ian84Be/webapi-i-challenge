7f86824bae1da42458c9912991b8c21ec39b3b93
Ian Be <ian84be@gmail.com>
2019-03-18 16:09:49 -0500

    add post function

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// implement your API here
+const express = require('express');
+const server = express();
+
+server.use(express.json()); // this makes POST and PUT work
+
+const db = require('./data/db.js'); // IMPORT the database
+
+server.post('/api/users', (req,res) => {
+    const newUser = req.body;
+    console.log(newUser);
+
+    if (newUser.name && newUser.bio) {
+        db.insert(newUser)
+        .then(user => {
+            res.status(201).json(user);
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: "There was an error while saving the user to the database"            });
+        });
+    } else {
+        res.status(400).json({
+            errorMessage: "Please provide name and bio for the user."
+        }).end();
+    }
+});
+
+server.listen(4000, () => {
+    console.log('listening on 4000')
+});
